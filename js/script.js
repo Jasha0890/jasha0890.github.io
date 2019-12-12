c33f8c9c36dbfc46b8ac593038ba113e05a87cbb
Jacopo Carraro <jasha0890@gmail.com>
2019-12-12 12:38:21 +0000

    Updated a script to scroll the page up
    scroll page up changed and updated

--- js/script.js
+++ js/script.js
@@ -186,17 +186,8 @@ $(document).ready(function() {
 	}
 
 	/*Scroll to top when arrow up clicked BEGIN*/
-	$(window).scroll(function() {
-	    var height = $(window).scrollTop();
-	    if (height > 100) {
-	        $('#back2Top').fadeIn();
-	    } else {
-	        $('#back2Top').fadeOut();
-	    }
-	});
-	
 	$(document).ready(function() {
-	    $("#back2Top").click(function(event) {
+	    $(".navbar-brand").click(function(event) {
 	        event.preventDefault();
 	        $("html, body").animate({ scrollTop: 0 }, "slow");
 	        return false;
